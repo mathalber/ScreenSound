2eec4838e36180454a1e5c9407565bfa1aabf1db
mathalber <mathalber@outlook.com>
2025-09-08 07:00:08 -0300

    [FEAT] Code improvement

--- ScreenSound/Album.cs
+++ ScreenSound/Album.cs
@@ -1,7 +1,11 @@
 class Album
 {
     private List<Musica> musicas = new List<Musica>();
-    public string? Nome { get; set; }
+    public Album(string nome)
+    {
+        Nome = nome;
+    }
+    public string? Nome { get; }
     public Genero? Genero { get; set; } 
     public int DuracaoTotal => musicas.Sum(m => m.Duracao);
 
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,19 +1,15 @@
 ﻿Genero rock = new Genero("Rock");
 
-Album albumDoQueen = new Album();
-albumDoQueen.Nome = "A Night at the Opera";
-albumDoQueen.Genero = rock;
+Banda queen = new Banda("Queen");
 
-Musica musica1 = new Musica();
-musica1.Nome = "Love of my life";
-musica1.Artista = "Queen";
+Album albumDoQueen = new Album("A Night at the Opera");
+
+Musica musica1 = new Musica(queen, "Love of my life");
 musica1.Duracao = 213;
 musica1.Disponivel = true;
 
 
-Musica musica2 = new Musica();
-musica2.Nome = "Bohemian Rhapsody";
-musica2.Artista = "Queen";
+Musica musica2 = new Musica(queen, "Bohemian Rhapsody");
 musica2.Duracao = 354;
 musica2.Disponivel = false;
 
@@ -21,7 +17,6 @@ musica2.Disponivel = false;
 albumDoQueen.AdicionarMusica(musica1);
 albumDoQueen.AdicionarMusica(musica2);
 
-Banda queen = new Banda();
-queen.Nome = "Queen";
+albumDoQueen.ExibirMusicasDoAlbum();
 queen.AdicionarAlbum(albumDoQueen);
 queen.ExibirDiscografia();
